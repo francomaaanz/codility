ea24ad8a335a26a0870f1745661f99a18a3339c6
francomaaanz <francomanz@hotmail.com>
2017-04-02 04:58:30 -0300

    cycling rotation

--- CyclicRotation.js
+++ CyclicRotation.js
@@ -2,20 +2,25 @@ function solution(A, K) {
 	var lengthA = A.length;
 	var newArray = [lengthA];
 	var newK = K % lengthA;
-	
+	console.log('newK', newK);
 	if(!A || A.length == 0 ){
 		return A;
 	}
 
 	for(var i = 0; i < lengthA; i++){
-		var max = i + newK;
-		 newArray[(i + newK)% lengthA] = A[i];
+		var sum = i + newK;
+		var finalIndex = (sum)% lengthA;
+		console.log('sum', sum);
+		console.log('newK', newK);
+		console.log('finalIndex', finalIndex);
+	 	newArray[finalIndex] = A[i];
 	}
 	return newArray;
-
 }
 
 var A =[3, 8, 9, 7, 6];
+// var A =[];
+// var A =[0];
 var Expected =[9, 7, 6, 3, 8];
 var K = 3;
 console.log('result => ', solution(A, K));
