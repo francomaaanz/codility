d1cbecff8a12493296632a7ac5a5a634dbd9be66
francomaaanz <francomanz@hotmail.com>
2017-08-04 01:07:59 -0300

    encontrar el elemento perdido

--- 2-arrays/oddOcurrencyInArrays.js
+++ 2-arrays/oddOcurrencyInArrays.js
@@ -1,10 +1,10 @@
-// function solution(A){
-//   var x = 0;
-//   for(var i = 0; i < A.length; i++) {
-//   		x ^= A[i];
-//   }
-//   return x;
-// }
+function solution(A){
+  var x = 0;
+  for(var i = 0; i < A.length; i++) {
+  		x ^= A[i];
+  }
+  return x;
+}
 
 var array = [];
 array[0] = 9;  
@@ -14,15 +14,5 @@ array[3] = 3;
 array[4] = 9;  
 array[5] = 7;
 array[6] = 9;
-array[7] = 45;
-// console.log('A => ', array)
-// console.log(solution(array))
-
-
-var numbers = [65, 44, 12, 4];
-
-function getSum(total, num) {
-	var x = 0
-    return x^=num;
-}
-console.log(array.reduce(getSum));
+console.log('A => ', array)
+console.log(solution(array))
--- 4-countingElements/MissingInteger.js
+++ 4-countingElements/MissingInteger.js
@@ -1,26 +1,32 @@
-function solution(A){
-	var aux;
+function solution(A) {
+	var n = A.length + 1;
+ 	var expectedSum = (n / 2) * (n + 1);
+ 	var sum = 0;
 
-	if(A.length == 1)	{
-		return A[0];
+	if(!A || A.length == 0){
+		return 1;
 	}
 
-	for(var i = 0; i < A.length - 1; i++){
-		aux ^= A[i];
-	}
-
-	return aux;
-
+	for (var i = 0; i < A.length; i++) {
+		sum += A[i];
+	};
+	return (expectedSum - sum);
 }
 
-var A = [];
-A[0] = 1;
-A[1] = 3;
-A[2] = 6;
-A[3] = 4;
-A[4] = 1;
-A[5] = 2;
+function solution(A) {
+    var xor = 0;
+    for (var i = 0; i < A.length; i++){
+        xor = xor^A[i]^(i + 1)
+    }        
+    return xor^(A.length + 1)
+}
 
-// The function should return 5.
-console.log(A);
+
+
+var A = [];
+A[0] = 2
+A[1] = 3
+A[2] = 1
+A[3] = 5
+
 console.log(solution(A));
